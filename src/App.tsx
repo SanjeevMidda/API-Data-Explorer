40150907d6afc65a36595d0adaeaaea30e71f6a2
Sanjeev Midda <s.midda1@gmail.com>
2026-05-04 14:35:09 +0100

    fix: moved appStatus state type into userFetchData hook for UI to be updated correctly

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,20 @@ import useFetchData from "./components/hooks/useFetchData";
 import { useState } from "react";
 
 function App() {
-  useFetchData("https://jsonplaceholder.typ");
+  const { data, appStatus } = useFetchData(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
 
-  type status = "loading" | "error" | "success";
-
-  const [appStatus, setAppStatus] = useState<status>("loading");
+  console.log(appStatus);
 
   return (
     <div className="App">
       <Header title="API Data Explorer" />
 
       <div className="mainPostContainer">
-        {appStatus === "loading" && <p>loading data...</p>}
-        {appStatus === "error" && <p>error loading data. Please try again</p>}
-        {appStatus === "success" && <p>data loaded!</p>}
+        {appStatus === "loading" && <p>Loading data...</p>}
+        {appStatus === "error" && <p>Error loading data. Please try again</p>}
+        {appStatus === "success" && <p>Data loaded!</p>}
       </div>
 
       <div className="searchAndRefreshContainer">
--- src/components/hooks/useFetchData.tsx
+++ src/components/hooks/useFetchData.tsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from "react";
 
+type status = "loading" | "error" | "success";
+
 const useFetchData = (url: string) => {
   const [data, setData] = useState(null);
+  const [appStatus, setAppStatus] = useState<status>("loading");
 
   useEffect(() => {
     const getData = async () => {
+      setAppStatus("loading");
       try {
         let response = await fetch(url);
         let datafromAPI = await response.json();
         setData(datafromAPI);
+        setAppStatus("success");
         console.log(datafromAPI);
       } catch (error) {
         console.log(error);
+        setAppStatus("error");
       }
     };
 
     getData();
   }, []);
-  return;
+  return { data, appStatus };
 };
 
 export default useFetchData;
