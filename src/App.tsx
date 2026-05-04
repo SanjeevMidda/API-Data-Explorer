137028fb65a99db07bc93c4a0ac97c7047e45602
Sanjeev Midda <s.midda1@gmail.com>
2026-05-04 13:25:58 +0100

    feature: created search UI component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./index.css";
 import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Header title="API Data Explorer" />
+
+      <div className="searchAndRefreshContainer">
+        <Search />
+      </div>
     </div>
   );
 }
