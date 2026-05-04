29c2c87e1cc8578aeb554150bbca1cfde548a0d0
Sanjeev Midda <s.midda1@gmail.com>
2026-05-04 13:40:18 +0100

    styles: created styling for Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Search = () => {
-  return <input placeholder="enter text"></input>;
+  return <input placeholder="Enter text"></input>;
 };
 
 export default Search;
