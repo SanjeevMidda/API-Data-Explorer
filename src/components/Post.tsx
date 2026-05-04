aa2e0d97ecad60e6b66c9ec162416960a2862094
Sanjeev Midda <s.midda1@gmail.com>
2026-05-04 15:29:56 +0100

    feat: added dropdown to Post component to show all data from post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
 import { APIData } from "../types/APIData";
 
 const Post = ({ body, id, title, userId }: APIData) => {
+  const [dropDown, setDropDown] = useState(false);
   return (
-    <div className="postContainer">
+    <div className="postContainer" onClick={() => setDropDown(!dropDown)}>
       <h3>
         <span>TITLE:</span> {title}
       </h3>
-      <p>
-        <span>POST:</span> {body}
-      </p>
-      <p>
-        <span>USERID:</span> {userId}
-      </p>
-      <p>
-        <span>ID:</span> {id}
-      </p>
+
+      {dropDown && (
+        <>
+          <p>
+            <span>POST:</span> {body}
+          </p>
+          <p>
+            <span>USERID:</span> {userId}
+          </p>
+          <p>
+            <span>ID:</span> {id}
+          </p>
+        </>
+      )}
     </div>
   );
 };
