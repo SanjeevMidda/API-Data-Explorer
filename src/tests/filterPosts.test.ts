bd29fa56ec258f911dc8baf60f8a3e3a984d1278
Sanjeev Midda <s.midda1@gmail.com>
2026-05-05 21:38:36 +0100

    feat: created mock data to test our filtering logic

---
+++ src/tests/filterPosts.test.ts
@@ -0,0 +1,8 @@
+import { filterPosts } from "../utils/filterPosts";
+import { APIData } from "../types/APIData";
+
+const mockPosts: APIData[] = [
+  { id: 1, title: "Hello World", body: "...", userId: 1 },
+  { id: 2, title: "React Testing", body: "...", userId: 2 },
+  { id: 3, title: "Jest Basics", body: "...", userId: 3 },
+];
