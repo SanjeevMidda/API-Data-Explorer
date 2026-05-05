558bb3032121175cf181bf5f2ab7e9f20feab6f9
Sanjeev Midda <s.midda1@gmail.com>
2026-05-05 21:57:00 +0100

    created test only posts which match the search term are shown

--- src/tests/filterPosts.test.ts
+++ src/tests/filterPosts.test.ts
@@ -2,13 +2,13 @@ import { filterPosts } from "../utils/filterPosts";
 import { APIData } from "../types/APIData";
 
 const mockPosts: APIData[] = [
-  { id: 1, title: "Hello World", body: "...", userId: 1 },
-  { id: 2, title: "React Testing", body: "...", userId: 2 },
-  { id: 3, title: "Jest Basics", body: "...", userId: 3 },
+  { id: 1, title: "Tom", body: "...", userId: 1 },
+  { id: 2, title: "John", body: "...", userId: 2 },
+  { id: 3, title: "Lisa", body: "...", userId: 3 },
 ];
 
 describe("filter posts", () => {
-  test("returns all posts when search is empty", () => {
+  test("should return all posts when search is empty", () => {
     const posts = mockPosts;
 
     const search = "";
@@ -16,5 +16,16 @@ describe("filter posts", () => {
     const result = filterPosts(posts, search);
 
     expect(result).toEqual(posts);
+  });
+
+  test("should return only posts that match the search term in the title", () => {
+    const posts = mockPosts;
+
+    const search = "John";
+
+    const result = filterPosts(posts, search);
+
+    const expected = [{ id: 2, title: "John", body: "...", userId: 2 }];
+    expect(result).toEqual(expected);
   });
 });
