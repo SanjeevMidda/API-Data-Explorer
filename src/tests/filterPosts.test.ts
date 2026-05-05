c478797210ee13245f0011090012da0e2f734a83
Sanjeev Midda <s.midda1@gmail.com>
2026-05-05 21:45:34 +0100

    created test to check for all posts to be displayed if no search term provided

--- src/tests/filterPosts.test.ts
+++ src/tests/filterPosts.test.ts
@@ -6,3 +6,15 @@ const mockPosts: APIData[] = [
   { id: 2, title: "React Testing", body: "...", userId: 2 },
   { id: 3, title: "Jest Basics", body: "...", userId: 3 },
 ];
+
+describe("filter posts", () => {
+  test("returns all posts when search is empty", () => {
+    const posts = mockPosts;
+
+    const search = "";
+
+    const result = filterPosts(posts, search);
+
+    expect(result).toEqual(posts);
+  });
+});
