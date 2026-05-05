edf2f6eb7e45a692cedc3e14067db49b76829b1e
Sanjeev Midda <s.midda1@gmail.com>
2026-05-05 21:19:18 +0100

    set up testing environment for Jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  preset: "ts-jest",
+
+  testEnvironment: "jsdom",
+
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
